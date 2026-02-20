e3015523d6966fe7ddb0dff39a250174ba624db1
i32mumaj <jorgemuniz2005@gmail.com>
2026-02-20 11:54:57 +0100

    Implemented basic Dish Class tests

--- src/abstractfactory/Main.java
+++ src/abstractfactory/Main.java
@@ -1,5 +1,12 @@
+package abstractfactory;
+
+import abstractfactory.DishType;
+import abstractfactory.Side;
+
 public class Main {
     public static void main(String[] args){
-        System.out.println("Test");
+        Dish myDish = new Dish("Macarrones", DishType.MAIN_COURSE, 5, Side.SALAD);
+
+        System.out.println(myDish.toString());
     }
 }
