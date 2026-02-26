d4619deda1d5971c2489c28c7e41851336a2e07f
i32mumaj <jorgemuniz2005@gmail.com>
2026-02-26 11:53:40 +0100

    renamed from main.java

--- src/adapter/Main.java
+++ src/adapter/Main.java
@@ -1,14 +1,10 @@
 package adapter;
 
-import adapter.BasicFormatter;
-
-public class main {
+public class Main {
     public static void main(String[] args){
         BasicFormatter bf = new BasicFormatter("src/resources/text.txt");
 
         String lineas = bf.extractText(2, 6);
-
-        bf.appendText(lineas);
 
         System.out.println(lineas);
 
